79dd77331497141acc33a6f75ba85bff68fd6c07
hudaokeji <github@hudaokeji.com>
2018-12-24 16:28:03 +0900

    init commit

---
+++ dist/skylark-angular/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-angular - A version of angularjs framework that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-angular/
+ * @license MIT
+ */
+define(["./angular"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
